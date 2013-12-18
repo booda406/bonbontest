b9315eb542e39af7be99762fe56ee46aaf66e36d
Steven Tseng <booda406@gmail.com>
2013-12-18 11:39:06 +0800

    add after sign in with login in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,4 +15,8 @@ class ApplicationController < ActionController::Base
   def ckeditor_filebrowser_scope(options = {})
     { :order => [:id, :desc], :user_id => current_user.id }.merge(options)
   end
+
+  def after_sign_in_path_for(resource)
+    request.env['omniauth.origin'] || stored_location_for(resource) || root_path
+  end
 end
