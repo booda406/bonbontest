7407d733a4f8bd26bcee17f261c0f43d1d0075ef
Steven Tseng <booda406@gmail.com>
2013-12-18 11:46:20 +0800

    add new def current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,4 +19,10 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     request.env['omniauth.origin'] || stored_location_for(resource) || root_path
   end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
+
 end
