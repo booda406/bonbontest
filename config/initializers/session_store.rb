0e9301f64ab4da587cbcf86d1766b0333e4ae319
Steven Tseng <booda406@gmail.com>
2013-12-18 14:28:17 +0800

    chg sessiong to cookie

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 #store in cookies limit 4kb
-# Bonbontest::Application.config.session_store :cookie_store, key: '_bonbontest_session'
+ Bonbontest::Application.config.session_store :cookie_store, key: '_bonbontest_session'
 
 #store in sessions table won't have limit
-Bonbontest::Application.config.session_store :active_record_store, key: '_bonbontest_session', :domain => 'loveturtle.herokuapp.com'
+#Bonbontest::Application.config.session_store :active_record_store, key: '_bonbontest_session', :domain => 'loveturtle.herokuapp.com'
